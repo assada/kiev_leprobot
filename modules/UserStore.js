364a332af2778e92b08698bd7092f1e211e753ff
Assada <assada.ua@gmail.com>
2017-07-07 12:23:57 +0300

    Test users

--- modules/UserStore.js
+++ modules/UserStore.js
@@ -8,16 +8,16 @@ module.exports = class UserStore {
     }
 
     store(msg) {
-
         this.User.sync().then(() => {
             // Table created
             return this.User.findOrCreate({
                 where: {
                     user: msg.from.id,
+                },
+                defaults: {
                     first_name: emojiStrip(msg.from.first_name),
                     last_name: emojiStrip(msg.from.last_name),
-                    username: emojiStrip(msg.from.username),
-                    language_code: msg.from.language_code
+                    username: emojiStrip(msg.from.username)
                 }
             });
         });
