e1f81273b19441b366c5d1443e95c6e69845d4fb
Assada <assada.ua@gmail.com>
2017-07-09 00:54:22 +0300

    [WIP] Pidor command

--- Generator/PidorGenerator.js
+++ Generator/PidorGenerator.js
@@ -10,18 +10,20 @@ module.exports = class PidorGenerator {
 
     get(msg) {
         const chat = msg.chat.id;
+        let pr = this.PidorRepository;
+        let ucr = this.UserChatRepository;
         return new Promise(function (fulfill, reject) {
-            this.PidorRepository.get(chat).then(function (res) {
+            pr.get(chat).then(function (res) {
                 if (res.length > 0) {
                     console.log(res);
                     reject('Rej 1');
                     //fulfill(res[0].dataValues);
                 } else {
-                    this.UserChatRepository.getActiveUser(chat).then(users => {
+                    ucr.getActiveUser(chat).then(users => {
                         if (users.length > 0) {
                             let user = users[Math.floor(Math.random() * users.length)];
                             console.log(user);
-                            this.PidorRepository.store(msg, user.dataValues.id);
+                            pr.store(msg, user.dataValues.id);
                             fulfill(user.dataValues);
                         } else {
                             reject('Rej 2');
