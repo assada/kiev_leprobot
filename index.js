c2209c582f94f5cc9b0da5534858c78beaa03cdd
Assada <assada.ua@gmail.com>
2017-07-08 10:55:21 +0300

    :zap: Bot top

--- index.js
+++ index.js
@@ -114,6 +114,10 @@ bot.onText(/\/top/, (msg, match) => {
     let sql = 'SELECT count(m.id) c, m.user, u.first_name, u.last_name from messages m LEFT JOIN users u ON m.user = u.user WHERE m.chat = '+msg.chat.id+' GROUP by m.user, u.first_name, u.last_name ORDER BY c DESC LIMIT 5';
 
     sequelize.query(sql).spread((results, metadata) => {
-        console.log(results);
+        let result = 'Топ 5 флудерастов: \n';
+        results.forEach(function (item) {
+            result += item.first_name + (item.last_name !== '' ? ' ' + item.last_name : '') + ' - ' + item.c + '\n';
+        });
+        bot.sendMessage(msg.chat.id, result);
     })
 });
