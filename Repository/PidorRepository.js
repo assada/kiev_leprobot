9e72b5e9c8de67aaa837796cdc4e5d2dab6eec65
Assada <assada.ua@gmail.com>
2017-07-09 01:17:22 +0300

    [WIP] Pidor command

--- Repository/PidorRepository.js
+++ Repository/PidorRepository.js
@@ -27,13 +27,17 @@ module.exports = class PidorRepository {
 
     get(chat) {
         this.Pidor.sync().then(() => {
+            let now= new Date().toISOString().slice(0, 19).replace('T', ' ');
+            let yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toISOString().slice(0, 19).replace('T', ' ');
+
             return this.Pidor.findAll({
                 /*include: [{
                  model: User,
                  where: {user: Sequelize.col('pidor.user')}
                  }],*/
                 where: {
-                    chat: chat
+                    chat: chat,
+                    updatedAt: {$between: [yesterday, now]},
                 },
                 order: [
                     ['id', 'DESC']
