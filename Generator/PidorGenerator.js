5eb036fef5bd18ce8679ab561e0b982bf827852f
Assada <assada.ua@gmail.com>
2017-07-08 23:05:03 +0300

    [WIP] Pidor command

--- Generator/PidorGenerator.js
+++ Generator/PidorGenerator.js
@@ -9,7 +9,7 @@ module.exports = class PidorGenerator {
     get(msg) {
         this.UserRepository.getActiveUser().then(users => {
             let user = users[Math.floor(Math.random() * users.length)];
-            console.log(user);
+            console.log(user.dataValues.username);
         }); //TODO: Promise
 
     }
