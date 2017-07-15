8efb4fd00568758f56e76628af172563c6f181ed
Assada <assada.ua@gmail.com>
2017-07-16 00:36:37 +0300

    :zap: News command

--- Generator/NewsGenerator.js
+++ Generator/NewsGenerator.js
@@ -6,10 +6,10 @@ module.exports = class NewsGenerator {
     get() {
         winston.info('Loading news');
         return new Promise(function (fulfill, reject) {
-            request({uri: 'https://www.ukr.net/news/kiev.html', method: 'GET', encoding: 'binary'},
+            request({uri: 'https://www.ukr.net/news/dat/kiev/1/', method: 'GET', encoding: 'binary'},
                 function (err, res, page) {
                     let result = [];
-                    let $ = cheerio.load(page);
+                    /*let $ = cheerio.load(page);
 
                     console.log(page);
 
@@ -22,7 +22,21 @@ module.exports = class NewsGenerator {
                             console.log(text + ': ' + link);
                             result.push({title: text, link: link})
                         }
+                    });*/
+
+                    let json = JSON.parse(page);
+
+                    let tops = json.tops;
+
+                    tops.filter(function (top) {
+                        return typeof top.Dops !== 'undefined';
                     });
+
+                    tops.forEach(function (topic) {
+                        result.push({title: topic.Title, link: topic.Url})
+                    });
+
+                    console.log(result);
 
                     fulfill(result);
 
