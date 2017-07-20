bcb8e7034ea2ce25e92d4b4278a807bf26842389
Assada <assada.ua@gmail.com>
2017-07-20 14:22:46 +0300

    :zap: Cache

--- Repository/ExchangeRatesRepository.js
+++ Repository/ExchangeRatesRepository.js
@@ -15,11 +15,13 @@ module.exports = class ExchangeRatesRepository {
         const t = this;
         return new this.Promise((result) => {
             if (t.cache.get('ExchangeRates') === null) {
+                console.log('creating cache');
                 t.request.get("https://openexchangerates.org/api/latest.json?app_id=" + token + '&show_alternative=1', function (err, res, body) {
                     t.cache.put('ExchangeRates', body, 43200000);
                     result(JSON.parse(body));
                 });
             } else {
+                console.log('using cache');
                 result(JSON.parse(t.cache.get('ExchangeRates')));
             }
         });
